b7ed437e8f416572f41cc2f60b94e7ef5c0f2c26
lilrebel17 <zackfarmer1710@gmail.com>
2023-12-30 00:19:42 -0500

    Logrotate fix
    - Application now rotates the log file every 24 hours instead of relying on linux logrotate
    - We use the date variable in the config.cfg file to do this
    - logger.py
      - rotate_log()
        - copies the current log file to a new file in the same directory named log_[month]-[day]-[year].log
        - then erases log.log
    - configer.py
      - set_date()
        - updated format for date variable from %m/%d/%y to %m-%d-%y
      - Added comments and run set_date on init
    - main.py
      - Added new check for log rotation
        - We compare config.cfg & the current time.
        - If its not the same, we rotate the log.
        - If it rotate successfully, we set the config file to the current date

--- modules/logger.py
+++ modules/logger.py
@@ -1,5 +1,9 @@
 import logging
-
+import os
+import datetime
+import shutil
+import time
+from modules.configer import configuration
 # Logger will handle anything dealing with the log file
 # This includes updating & reading the log file.
 
@@ -7,7 +11,6 @@ class Logger():
     def __init__(self) -> None:
         self.format = '[%(asctime)s] - %(message)s'
         self.log_path = './logs/log.log'
-        
         logging.basicConfig(level=logging.INFO,format=self.format,datefmt='%m/%d/%Y %I:%M:%S %p',filename=self.log_path,encoding='utf-8')
         logging.basicConfig(level=logging.ERROR,format=self.format,datefmt='%m/%d/%Y %I:%M:%S %p',filename=self.log_path,encoding='utf-8')
 
@@ -29,12 +32,36 @@ class Logger():
 
     #Used as the email body when the site is unreachable.
     def get_last_log(self):
-        with open(self.log_path,"r") as log:
-            #We split the lines to see how many their are in the file
-            lines = log.read().splitlines()
-            #Finally, we take that number of lines and -1 due to it counting the empty space at the end.
-            last_line = lines[-1]
-            return last_line
+        try:
+            with open(self.log_path,"r") as log:
+                #We split the lines to see how many their are in the file
+                lines = log.read().splitlines()
+                #Finally, we take that number of lines and -1 due to it counting the empty space at the end.
+                last_line = lines[-1]
+                log.close()
+                return last_line
+        except IndexError as error:
+            error_string = str(error)
+            self.error(error_string)
+            return False
 
+    #This is meant to rotate the log file every 24 hours a common naming schema
+    #Log file Naming Schema:
+    #log_month-day-year.log
+    def rotate_log(self):
+        #Get the paths for the log folder and what will be yesterdays log
+        log_folder = os.path.dirname('./logs/log.log')
+        yesterday_log = f'log_{configuration.date}.log'
+
+        try:
+            #Then we copy everything from the current log file to yesterdays log
+            shutil.copyfile(self.log_path,f'{log_folder}/{yesterday_log}')
+        except Exception as e:
+            self.error(e)
+        else:
+            #If the file copies to yesterdays log. We erase the current log.log.
+            current_log = open(self.log_path,'+r')
+            current_log.truncate(0)
+            current_log.close()
 
 logger = Logger()
